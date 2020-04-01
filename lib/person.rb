60aa6d5da920543ceb17bd90b759dca398cc714f
Jorge Ramirez <joramirez96@gmail.com>
2020-04-01 22:31:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,20 +1,20 @@
 class Person
   def name=(name)
-    @name = name 
-  end 
+    @name = name
+  end
 
-  def name 
-    @name 
-  end 
+  def name
+    @name
+  end
 
 
   def job=(job)
-    @job = job 
-  end 
+    @job = job
+  end
 
-  def job 
-    @job 
-  end 
-  
-  
-end 
+  def job
+    @job
+  end
+
+
+end
